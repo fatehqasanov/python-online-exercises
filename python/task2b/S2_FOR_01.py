92717644dc26eee4f8c7f9ab01473971d28cdd6e
FatehMook <fatehqasanov@gmail.com>
2026-01-20 21:55:47 +0100

    TASK2B: S2_FOR_01 - print FizzBuzz up to 30 using loop

--- python/task2b/S2_FOR_01.py
+++ python/task2b/S2_FOR_01.py
@@ -1,12 +1,15 @@
-"""
-S2_FOR_01.py
-Auto-generated template.
-Fill in the implementation according to the exercise description.
-"""
-
 def main():
-    # TODO: implement exercise
-    pass
+    n = 30
+    for i in range(1, n + 1):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
 
 if __name__ == "__main__":
     main()
