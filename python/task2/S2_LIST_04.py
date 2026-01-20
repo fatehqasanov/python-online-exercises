54f2c3911f7aeae597150a7837d47d5d58e4d2c3
FatehMook <fatehqasanov@gmail.com>
2026-01-20 21:54:43 +0100

    TASK2: S2_LIST_04 - flatten list by exactly one level

--- python/task2/S2_LIST_04.py
+++ python/task2/S2_LIST_04.py
@@ -1,12 +1,18 @@
-"""
-S2_LIST_04.py
-Auto-generated template.
-Fill in the implementation according to the exercise description.
-"""
+def flatten1(lst):
+    result = []
+    for item in lst:
+        if isinstance(item, list):
+            for x in item:
+                result.append(x)
+        else:
+            result.append(item)
+    return result
+
 
 def main():
-    # TODO: implement exercise
-    pass
+    print(flatten1([1, [2, 3], 4, [5]]))
+    print(flatten1([[1, 2], [], [3], 4]))
+
 
 if __name__ == "__main__":
     main()
